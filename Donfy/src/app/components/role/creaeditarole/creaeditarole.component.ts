eae34a88ab816298045927f62dec1830f6015310
Golreddd <rquispegiraldo@gmail,com>
2024-11-05 01:20:06 -0500

    Services insert Roles

--- Donfy/src/app/components/role/creaeditarole/creaeditarole.component.ts
+++ Donfy/src/app/components/role/creaeditarole/creaeditarole.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { Role } from '../../../models/Role';
+import { RoleService } from '../../../services/role.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-creaeditarole',
@@ -16,13 +19,30 @@ import { MatInputModule } from '@angular/material/input';
 })
 export class CreaeditaroleComponent implements OnInit {
   form: FormGroup = new FormGroup({});
+  ro:Role=new Role();
+
+  constructor(private formBuilder:FormBuilder, private rS:RoleService, private router:Router){}
   
   ngOnInit(): void {
-      
+      this.form=this.formBuilder.group({
+        hrol:['',Validators.required],
+        husername:['',Validators.required],
+      });
   }
 
-  aceptar(){
-    
+  aceptar(): void{
+    if(this.form.valid){
+      this.ro.rol=this.form.value.hrol
+      this.ro.user.id=this.form.value.husername
+      this.rS.insert(this.ro).subscribe((data)=>{
+        this.rS.list().subscribe((data)=>{
+          this.rS.setList(data);
+        });
+      });
+      //redireccione a la lista
+      this.router.navigate(['Roles'])
+    }
+
   }
 
 }
