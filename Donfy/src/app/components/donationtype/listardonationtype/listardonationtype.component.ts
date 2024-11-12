ec7df9936cad61bafa912460bf71b8d2c3dcdb11
Golreddd <rquispegiraldo@gmail,com>
2024-11-12 18:50:38 -0500

    Paginator y Contador de Registro en Listar Tipo de Donativos

--- Donfy/src/app/components/donationtype/listardonationtype/listardonationtype.component.ts
+++ Donfy/src/app/components/donationtype/listardonationtype/listardonationtype.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { DonationType } from '../../../models/DonationType';
 import { DonationtypeService } from '../../../services/donationtype.service';
 import { RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-listardonationtype',
@@ -13,15 +14,19 @@ import { MatIcon } from '@angular/material/icon';
     MatTableModule,
     RouterModule,
     MatButtonModule,
-    MatIcon
+    MatIcon,
+    MatPaginatorModule
   ],
   templateUrl: './listardonationtype.component.html',
   styleUrls: ['./listardonationtype.component.css']
 })
 export class ListardonationtypeComponent implements OnInit {
   dataSource:MatTableDataSource<DonationType>=new MatTableDataSource();
+  @ViewChild(MatPaginator) Paginator!: MatPaginator;
+
 
   displayedColumns: string[]=['c1','c2','accion01','accion02']
+  totalRegistros: number = 0;
 
   constructor(private dtS:DonationtypeService){}
 
@@ -30,13 +35,21 @@ export class ListardonationtypeComponent implements OnInit {
       // Ordena los datos por idTipoDonation de forma ascendente
       data.sort((a, b) => a.idTipoDonation - b.idTipoDonation);
       this.dataSource = new MatTableDataSource(data);
+      this.totalRegistros = data.length;
+      this.dataSource.paginator = this.Paginator;
     });
     
     this.dtS.getList().subscribe(data => {
       // Ordena los datos por idTipoDonation de forma ascendente
       data.sort((a, b) => a.idTipoDonation - b.idTipoDonation);
       this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.Paginator;
+      this.totalRegistros = data.length; // Actualizar el total de registros aquí
     });
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.Paginator;
   }
 
   eliminar(id: number) {
@@ -45,6 +58,7 @@ export class ListardonationtypeComponent implements OnInit {
         // Ordena nuevamente la lista después de la eliminación
         data.sort((a, b) => a.idTipoDonation - b.idTipoDonation);
         this.dataSource = new MatTableDataSource(data);
+        this.totalRegistros = data.length; // Actualizar el total de registros aquí
       });
     });
   }
