820a3bf9bd71cdf298698f8e686dc7a966d7cce4
MoralesSil <Angiemoralessilverio2@gmail.com>
2024-11-12 10:19:33 -0500

    Update de funcionalidades OK

--- Donfy/src/app/app.routes.ts
+++ Donfy/src/app/app.routes.ts
@@ -18,7 +18,6 @@ import { CreaeditaroleComponent } from './components/role/creaeditarole/creaedit
 import { seguridadGuard } from './guard/seguridad.guard';
 import { FormulariopagoComponent } from './components/formulariopago/formulariopago.component';
 
-<<<<<<< HEAD
 export const routes: Routes = 
 [
     {
@@ -117,128 +116,18 @@ export const routes: Routes =
         component: FormulariopagoComponent,
         canActivate: [seguridadGuard],
     },
+    {
+        path: 'Notifications',
+        component: NotificationComponent,
+        children: [
+            {
+                path: 'Agregar',
+                component: CreaeditanotificationComponent
+            },
+            {
+                path: 'Modificar/:id',
+                component: CreaeditanotificationComponent
+            }
+        ]
+    },
 ];
-=======
-export const routes: Routes =
-    [
-        {
-            path: '',
-            redirectTo: 'landing',
-            pathMatch: 'full',
-        },
-        {
-            path: 'landing',
-            component: LandingComponent
-        },
-        {
-            path: 'NotificationType',
-            component: NotificationtypeComponent,
-            children: [
-                {
-                    path: 'New',
-                    component: CreaeditaNotificationtypeComponent
-                },
-                {
-                    path: 'Ediciones/:id',
-                    component: CreaeditaNotificationtypeComponent
-                }
-            ],
-            canActivate: [seguridadGuard],
-        },
-        {
-            path: 'Users',
-            component: UserComponent,
-            children: [
-                {
-                    path: 'New',
-                    component: CreaeditaUserComponent
-                },
-                {
-                    path: 'Ediciones/:id',
-                    component: CreaeditaUserComponent
-                }
-            ],
-            canActivate: [seguridadGuard],
-        },
-        {
-            path: 'login',
-            component: LoginComponent,
-        },
-        {
-            path: 'registrar',
-            component: RegisterComponent,
-        },
-        {
-            path: 'DonationType',
-            component: DonationtypeComponent,
-            canActivate: [seguridadGuard],
-        },
-        {
-            path: 'DonationType/listar',
-            component: ListardonationtypeComponent,
-            canActivate: [seguridadGuard],
-        },
-        {
-            path: 'DonationType/nuevo',
-            component: CreaeditadonationtypeComponent,
-            canActivate: [seguridadGuard],
-        },
-        {
-            path: 'DonationType/editar/:id',
-            component: CreaeditadonationtypeComponent,
-            canActivate: [seguridadGuard],
-        },
->>>>>>> d51611bc9dbc44080cd0bfe65f1671832ef1727f
-
-        {
-            path: 'Donations',
-            component: DonationComponent,
-            children: [
-                {
-                    path: 'nuevo', component: CreaeditadonationComponent
-                },
-                {
-                    path: 'Edit/id', component: CreaeditadonationComponent
-                }
-            ],
-            canActivate: [seguridadGuard],
-        },
-        {
-            path: 'Roles',
-            component: RoleComponent,
-            children: [
-                {
-                    path: 'nuevo', component: CreaeditaroleComponent
-                }
-            ],
-            canActivate: [seguridadGuard],
-        },
-        {
-            path: 'Donations',
-            component: DonationComponent,
-            children: [
-                {
-                    path: 'nuevo',
-                    component: CreaeditadonationComponent
-                },
-                {
-                    path: 'Edit/:id',
-                    component: CreaeditadonationComponent
-                }
-            ]
-        },
-        {
-            path: 'Notifications',
-            component: NotificationComponent,
-            children: [
-                {
-                    path: 'Agregar',
-                    component: CreaeditanotificationComponent
-                },
-                {
-                    path: 'Modificar/:id',
-                    component: CreaeditanotificationComponent
-                }
-            ]
-        },
-    ];
