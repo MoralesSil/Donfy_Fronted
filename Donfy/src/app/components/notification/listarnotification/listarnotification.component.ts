ebd542875b7596c5721f7dcd2ba39b3f094f0be4
Camila Fernanda Morales Farias <118644922+CamMorales-v25@users.noreply.github.com>
2024-11-19 02:26:08 -0500

    Listar por username

--- Donfy/src/app/components/notification/listarnotification/listarnotification.component.ts
+++ Donfy/src/app/components/notification/listarnotification/listarnotification.component.ts
@@ -4,6 +4,7 @@ import { NotificationsService } from '../../../services/notifications.service';
 import { MatIconModule } from '@angular/material/icon';  // Importar MatIconModule
 import { Router, RouterLink } from '@angular/router';
 import { Notifications } from '../../../models/Notifications';
+import { LoginService } from '../../../services/login.service';
 
 @Component({
   selector: 'app-listarnotification',
@@ -18,18 +19,42 @@ import { Notifications } from '../../../models/Notifications';
 })
 export class ListarnotificationComponent {
   notifications: Notifications[] = []; // Array de notificaciones
+  userRole: string='';
+  username: string='';
 
   constructor(
     private notificationsService: NotificationsService,
-    private router: Router
+    private router: Router,
+    public loginService:LoginService,
+    private notificationService:NotificationsService
   ) {}
 
   ngOnInit(): void {
-    // Cargar la lista de notificaciones al inicio
+    
+    this.userRole = this.loginService.showRole();
+    this.username = this.loginService.showUsername();
+
     this.notificationsService.list().subscribe((data) => {
       this.notifications = data;
     });
+
+    if (this.username) {
+      if (this.userRole === 'DONADOR') {
+        this.notificationService
+          .getNotificationsForUsername(this.username)
+          .subscribe((data) => {
+            this.notifications = data;
+          });
+      } else if (this.userRole === 'ONG') {
+        this.notificationService
+          .getNotificationsForUsername(this.username)
+          .subscribe((data) => {
+            this.notifications = data;
+          });
+      }
+    }
   }
+  
 
   eliminar(id: number): void {
     if (confirm('¿Estás seguro de que deseas eliminar esta notificación?')) {
@@ -38,8 +63,9 @@ export class ListarnotificationComponent {
       });
     }
   }
-
+  
   agregarNotificacion(): void {
     this.router.navigate(['/Notifications/agregar']);
   }
+
 }
